93d3bb21e477753b97dd137671e0fc4e9b72ce94
Min Yang <myangvgo@hotmail.com>
2023-04-28 15:42:05 +0800

    add simple queue

---
+++ Receive/Receive.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory
+{
+    HostName = "192.168.26.211",
+    Port = 5672,
+    UserName = "sadmin",
+    Password = "sadmin",
+    VirtualHost = "/"
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, evtArgs) => 
+{
+    var body = evtArgs.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received message {message}");
+};
+
+channel.BasicConsume(
+    queue: "hello",
+    autoAck: true,
+    consumer: consumer
+);
+
+Console.WriteLine("Press Enter to exit.");
+Console.ReadKey(false);
